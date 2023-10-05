87d956398045470b4a587ecc1988782505893299
haron <haronxlee@gmail.com>
2023-10-06 00:53:40 +0900

    Design: UI components 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,16 @@
+import Layout from '@/styles/Layout';
+import Header from 'components/header/header';
+import StreamDeck from 'components/body/StreamDeck';
+import Footer from 'components/footer/Footer';
+
 function App() {
-  return <div className='w-96 text-white bg-blue-500 rounded'>hi</div>;
+  return (
+    <Layout>
+      <Header />
+      <StreamDeck />
+      <Footer />
+    </Layout>
+  );
 }
 
 export default App;
