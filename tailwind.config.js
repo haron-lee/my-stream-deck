93bc090573f44e30bfdeb8064025f13bbaf44f20
haron <haronxlee@gmail.com>
2023-10-06 00:50:23 +0900

    Chore: tailwind color, boxShadow custom 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#1A1A1A',
+        bg: '#242424',
+      },
+      boxShadow: {
+        'stream-inner': 'inset 0 0px 10px 0 rgb(255 255 255 / 0.05)',
+      },
+    },
   },
   plugins: [],
 };
