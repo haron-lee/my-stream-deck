af22ef07da35c17a270ba2e3ace5241cccc2779e
haron <haronxlee@gmail.com>
2023-10-06 00:52:41 +0900

    Design: body stream deck UI

---
+++ src/components/body/StreamBtn.tsx
@@ -0,0 +1,7 @@
+export default function StreamBtn() {
+  return (
+    <button className='w-16 h-16 border border-neutral-700 rounded-xl shadow-stream-inner'>
+      <img src='' alt='' />
+    </button>
+  );
+}
---
+++ src/components/body/StreamDeck.tsx
@@ -0,0 +1,13 @@
+import StreamBtn from 'components/body/StreamBtn';
+
+export default function StreamDeck() {
+  return (
+    <div className='p-5 flex gap-5 justify-center flex-wrap'>
+      <StreamBtn />
+      <StreamBtn />
+      <StreamBtn />
+      <StreamBtn />
+      <StreamBtn />
+    </div>
+  );
+}
