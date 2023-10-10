a9207b2d27d66fc58fa7cea2a1e0a2dbedaf8b1a
haron <haronxlee@gmail.com>
2023-10-10 22:06:56 +0900

    Feat: 투명도 조절 기능 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import Layout from '@/styles/Layout';
-import Header from 'components/header/header';
-import StreamDeck from 'components/body/StreamDeck';
+import Header from 'components/header/Header';
+import StreamDeck from '@/components/streamDeck/StreamDeck';
 import Footer from 'components/footer/Footer';
+import GlobalStyles from '@/styles/GlobalStyles';
 
 function App() {
+  const [opacityCtrl, setOpacityCtrl] = useState(0.5);
+  const [deckState, setDeckState] = useState(10);
+  const changeOpacity = (num: number) => setOpacityCtrl(num);
+
   return (
-    <Layout>
-      <Header />
-      <StreamDeck />
-      <Footer />
-    </Layout>
+    <>
+      <GlobalStyles />
+      <Layout opacityCtrl={opacityCtrl}>
+        <Header />
+        <StreamDeck deckState={deckState} />
+        <Footer opacityCtrl={opacityCtrl} changeOpacity={changeOpacity} />
+      </Layout>
+    </>
   );
 }
 
