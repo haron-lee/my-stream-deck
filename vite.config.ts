6a1cabbad1e719252f38771d949b7840044ef7c1
haron <haronxlee@gmail.com>
2023-10-10 22:01:35 +0900

    Chore: vite twin macro 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,21 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
     tsConfigPaths(),
     electron({
       main: {
