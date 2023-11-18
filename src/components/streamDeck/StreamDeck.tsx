3724fd844c1a5a4b9115521c41478e0668eac0c7
haron <haronxlee@gmail.com>
2023-11-18 21:23:15 +0900

    Feat: streamBtns map으로 렌더링, saveInfo로 버튼들 추가

--- src/components/streamDeck/StreamDeck.tsx
+++ src/components/streamDeck/StreamDeck.tsx
@@ -1,15 +1,44 @@
-import StreamBtn from '@/components/streamDeck/StreamBtn';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import tw from 'twin.macro';
+import StreamBtn from 'components/streamDeck/StreamBtn';
+import { StreamButton } from 'components/streamDeck/StreamBtn';
+import InfoModal from 'components/modal/InfoModal';
+import PlusIcon from 'assets/icon-add.svg';
 
 type StreamDeckProps = {
-  deckState: number;
+  deckState?: number;
 };
 
 export default function StreamDeck({ deckState }: StreamDeckProps) {
-  const streamBtns = Array.from({ length: deckState }, (_, idx) => (
-    <StreamBtn key={idx} />
-  ));
-  return <StreamDeckWrapper>{streamBtns}</StreamDeckWrapper>;
+  const [streamBtns, setStreamBtns] = useState<JSX.Element[]>([]);
+  const [showInfoModal, setShowInfoModal] = useState(false);
+
+  const addStreamBtn = () => {
+    setShowInfoModal(true);
+  };
+
+  const saveInfo = (newBtn: JSX.Element) => {
+    setStreamBtns((prev) => [...prev, newBtn]);
+    setShowInfoModal(false);
+  };
+
+  return (
+    <StreamDeckWrapper>
+      {streamBtns &&
+        streamBtns.map((streamBtn, index) => {
+          return <React.Fragment key={index}>{streamBtn}</React.Fragment>;
+        })}
+      <AddButton onClick={addStreamBtn} />
+      {showInfoModal && (
+        <InfoModal
+          isOpen={showInfoModal} // 삭제 예정
+          onClose={() => setShowInfoModal(false)}
+          saveInfo={saveInfo}
+        />
+      )}
+    </StreamDeckWrapper>
+  );
 }
 
 const StreamDeckWrapper = tw.div`
@@ -18,4 +47,8 @@ const StreamDeckWrapper = tw.div`
   gap-5 
   justify-center
   flex-wrap
+`;
+
+const AddButton = styled(StreamButton)`
+  background: url(${PlusIcon}) no-repeat center/40%;
 `;
